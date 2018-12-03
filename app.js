ec557c429323843c92f286303ddab041a9541916
Daniel <hoptond848@protonmail.com>
2018-12-03 16:55:42 +0000

    lay out routes

--- app.js
+++ app.js
@@ -8,6 +8,19 @@ const port = 8080
 
 app.get('/', (req, res) => {
     res.send("This is where our todo app will go once we've ported it over")
+    //TODO: retrieve todos from database
+    //TODO: load template
+    //TODO: put todos into template
+})
+
+app.post('/', (req, res) => {
+    //TODO: get parsed TODO updates from form on page
+    //TODO: update each entry in the database
+})
+
+app.put('/', (req, res) => {
+    //TODO: get parsed update from NEW form
+    //TODO: insert new todo into the database
 })
 
 app.listen(port, ()=> {
