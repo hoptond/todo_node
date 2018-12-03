32770a84a173e0bcf528dac82c170e2c724ba5cd
Daniel <hoptond848@protonmail.com>
2018-12-03 17:01:21 +0000

    combine routes into one function

--- app.js
+++ app.js
@@ -5,23 +5,20 @@ const app = express()
 const port = 8080
 
 
-
-app.get('/', (req, res) => {
-    res.send("This is where our todo app will go once we've ported it over")
-    //TODO: retrieve todos from database
-    //TODO: load template
-    //TODO: put todos into template
-})
-
-app.post('/', (req, res) => {
-    //TODO: get parsed TODO updates from form on page
-    //TODO: update each entry in the database
-})
-
-app.put('/', (req, res) => {
+app.route('/')
+    .get((req, res) => {
+        res.send("This is where our todo app will go once we've ported it over")
+        //TODO: retrieve todos from database
+        //TODO: load template
+        //TODO: put todos into template
+        //TODO: send to user
+    }).post((req, res) => {
+        //TODO: get parsed TODO updates from form on page
+        //TODO: update each entry in the database
+    }).put((req, res) => {
     //TODO: get parsed update from NEW form
     //TODO: insert new todo into the database
-})
+    })
 
 app.listen(port, ()=> {
   console.log('node server running')
