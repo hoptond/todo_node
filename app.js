9fb775a56952108a7dfe5b9c86662c25a7e2434d
Daniel <hoptond848@protonmail.com>
2018-12-04 09:47:03 +0000

    rework routes

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const app = express()
 
 const port = 8080
 
-
-app.route('/')
+app.route('/todos')
     .get((req, res) => {
         res.send("This is where our todo app will go once we've ported it over")
         //TODO: retrieve todos from database
@@ -13,12 +12,18 @@ app.route('/')
         //TODO: put todos into template
         //TODO: send to user
     }).post((req, res) => {
-        //TODO: get parsed TODO updates from form on page
-        //TODO: update each entry in the database
-    }).put((req, res) => {
         //TODO: get parsed update from NEW form
         //TODO: insert new todo into the database
     })
+
+app.put('/todos/:id', (req, res)=> {
+    //TODO: get parsed TODO updates from form on page
+    //TODO: update the todo's status in the database
+})
+
+app.get('/todos?completed=1', (req, res) => {
+    //TODO: only show completed todos
+})
 
 app.listen(port, ()=> {
   console.log('node server running')
