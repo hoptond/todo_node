994b9f5d8a1bea3c060ab393525609f3e0d8d000
Daniel <hoptond848@protonmail.com>
2018-12-04 10:46:06 +0000

    copy css from old project into public folder and copy old html into handlebars template

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const port = 8080
 
 app.engine('handlebars',exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
+app.use(express.static('public'))
 
 app.get('/', (req, res)=> {
     res.redirect('/todos')
