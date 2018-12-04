d5bd030dc5dbd77b9b190cc529fb213fe47e4346
Daniel <hoptond848@protonmail.com>
2018-12-04 11:50:38 +0000

    remove whitespcae

--- app.js
+++ app.js
@@ -12,9 +12,6 @@ const connection = mysql.createConnection({
     database: 'todo'
 })
 
-
-
-
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
