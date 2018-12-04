2385bf6a8725868b7bf737658c6349441aee8f88
Daniel <hoptond848@protonmail.com>
2018-12-04 16:12:29 +0000

    replace mysql with mongo

--- app.js
+++ app.js
@@ -1,37 +1,54 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const mysql = require('mysql')
+const mongoClient = require('mongodb').MongoClient
+const mongoUrl = 'mongodb://localhost:27017/'
+const assert = require('assert')
+const bodyParser = require('body-parser')
 
 const app = express()
 
 const port = 8080
 
-const connection = mysql.createConnection({
-    host: '192.168.20.20',
-    user: 'root',
-    database: 'todo'
-})
+var client = new mongoClient(mongoUrl)
+
+const jsonParser = bodyParser.json()
+
+const urlencondedParser = bodyParser.urlencoded({extended: false})
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
-app.get('/', (req, res)=> {
+app.get('/', (req, res) => {
     res.redirect('/todos')
 })
 
 app.route('/todos')
     .get((req, res) => {
-        connection.query('SELECT `id`, `desc`, `status` FROM tasks', function (error, results, fields) {
-            if(error) {
-                throw error
-            }
-            res.render('home' , {results})
+        client.connect(function (err, client) {
+            const db = client.db('todo')
+            let results = db.collection('tasks').find({}).toArray(function (err, docs) {
+                console.log(docs)
+                if(docs.length > 0) {
+                    res.render('home', {tasks: docs})
+                }
+            })
         })
-    }).post((req, res) => {
-        //TODO: get parsed update from NEW form
-        //TODO: insert new todo into the database
     })
+
+app.route('/todos/new')
+    .post(urlencondedParser, (req, res)=> {
+    client.connect(function (err, client) {
+        const db = client.db('todo')
+        console.log(req.body)
+        db.collection('tasks').insertOne({desc: req.body.desc, status: 0}, (err, r)=>{
+            assert.equal(err, null)
+            assert.equal(1, r.insertedCount)
+            console.log('inserted stuff into db')
+            res.redirect('/todos')
+        })
+    })
+})
 
 app.put('/todos/:id', (req, res)=> {
     //TODO: get parsed TODO updates from form on page
