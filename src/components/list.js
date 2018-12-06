ab99e22a64e2e0720e1a0f0fdfeb5822876f50ca
Daniel <hoptond848@protonmail.com>
2018-12-06 11:21:59 +0000

    remove whitespace in list component

--- src/components/list.js
+++ src/components/list.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 
 class List extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            //tasks is an array of objects, each with an id and desc properties
+            tasks: props.tasks
+        };
+    }
+
     render() {
         return (
             <div>
