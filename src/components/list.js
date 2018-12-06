dfc0b3029477d5034008465afadfde1b69750ca8
Daniel <hoptond848@protonmail.com>
2018-12-06 10:56:34 +0000

    begin handlebars removal

---
+++ src/components/list.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class List extends Component {
+    render() {
+        return (
+            <div>
+                <ul className="List">
+                    {<li>test</li>}
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default List
+
+            
