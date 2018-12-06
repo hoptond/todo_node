56e2a6e6ed88705fb9f73470200429ecd8de4d1a
Daniel <hoptond848@protonmail.com>
2018-12-06 11:20:49 +0000

    add constructor to task

--- src/components/task.js
+++ src/components/task.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 
 class Task extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            //tasks is an array of objects, each with an id and desc properties
+            id: props.id,
+            desc: props.desc
+        };
+    }
+
     render() {
         return (
             <li>
-                <input type="checkbox" id={this._id} name={this._id}/>
-                    <span>{this.desc}</span>
+                <input type="checkbox" id={this.state.id} name={this.state.id}/>
+                    <span>{this.state.desc}</span>
             </li>
         );
     }
