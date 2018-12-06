a928de0b6f52ba727fa127c7ccac43dfbc451a2d
Daniel <hoptond848@protonmail.com>
2018-12-06 11:35:14 +0000

    remove incorrect comment from Task constructor

--- src/components/task.js
+++ src/components/task.js
@@ -4,7 +4,6 @@ class Task extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            //tasks is an array of objects, each with an id and desc properties
             id: props.id,
             desc: props.desc
         };
